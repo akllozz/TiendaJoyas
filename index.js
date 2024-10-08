40af39eb455b7ff9ab37f119bb67147e1f6289cc
Anakaren Lozano <aklozzg@gmail.com>
2024-10-07 20:04:08 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const {obtenerJoyas, obtenerJoyasFiltro, prepararHateoas} = require('./consulta');
+const format = require('pg-format');
+const app = express();
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
+
+app.get("/joyas", async (req, res) => { 
+try {
+    const queryStrings = req.query;
+    const { results: inventario } = await obtenerJoyas(queryStrings); 
+    const HATEOAS = await prepararHateoas(inventario);
+    res.json({ inventario, HATEOAS });
+    
+} catch (error) {
+    console.error("Error al obtener las joyas:", error);
+    res.status(500).json({ error: "Error al obtener las joyas" });
+}}
+);
+
+app.get("/joyas/filtros", async (req, res) => {
+    try {
+      const queryStrings = req.query;
+      const inventario = await obtenerJoyasFiltro(queryStrings);
+      res.json(inventario);
+    } catch (error) {
+      console.error("Error al obtener las joyas:", error);
+      res.status(500).json({ error: "Error al obtener las joyas" });
+    }
+  });
